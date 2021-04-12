626e4ba0cab678e91e944cd7765a3a14b0160056
Spencer Gillette <spencer.gillette@revature.net>
2021-04-12 10:53:26 -0400

    add first package

---
+++ src/main/java/com/revature/app/Application.java
@@ -0,0 +1,24 @@
+package com.revature.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.javalin.Javalin;
+
+public class Application {
+	
+	private static Javalin app;
+	private static Logger logger = LoggerFactory.getLogger(Application.class);
+	
+	public static void main(String[] args) {
+		app = Javalin.create();
+		
+		app.before(ctx -> {
+			String URI = ctx.req.getRequestURI();
+			String httpMethod = ctx.req.getMethod();
+			logger.info(httpMethod + " request to endpoint " + URI + "GET received");
+		});
+
+	}
+
+}
